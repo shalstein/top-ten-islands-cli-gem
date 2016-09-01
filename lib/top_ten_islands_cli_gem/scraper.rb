e090f8bcd4263345c83c64f526a7e6e49c9ce645
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-09-01 15:33:45 +0000

    added to scraper get top islands method

--- lib/top_ten_islands_cli_gem/scraper.rb
+++ lib/top_ten_islands_cli_gem/scraper.rb
@@ -1,12 +1,13 @@
 class TopTenIslandsCliGem::Scraper
 
-  def get_top_islands
-    html = Nokogiri::HTML(open("https://www.tripadvisor.com/TravelersChoice-Islands"))
-    binding.pry
+  def get_page
+    Nokogiri::HTML(open("https://www.tripadvisor.com/TravelersChoice-Islands"))
   end
 
-  def test
-    puts "great!"
+  def get_top_islands
+    self.get_page.css(" .mainName").map do |island|
+       island.text.strip
+    end
   end
 
 end
