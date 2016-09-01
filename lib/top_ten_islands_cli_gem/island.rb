d2cae63547d547e54a79633567d10f91b2bee4d1
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-09-01 18:14:56 +0000

    added get_discription function

--- lib/top_ten_islands_cli_gem/island.rb
+++ lib/top_ten_islands_cli_gem/island.rb
@@ -2,9 +2,25 @@ class TopTenIslandsCliGem::Island
 
   attr_accessor :name, :url, :discription
 
+  ALL = []
+
   def initialize(name=nil, url=nil)
     @name = name
     @url = url
+    ALL.push(self)
   end
+
+  def get_discription
+    binding.pry
+    island_page = Nokogiri::HTML(open(@url))
+    @discription = island_page.css(" .ermb_text").css(" .content").text.strip
+  end
+
+  def self.all
+    ALL
+  end
+
+
+
 
 end
