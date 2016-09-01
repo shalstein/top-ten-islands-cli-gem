9e7ce353f33029be86ab621687af256ecc016c25
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-09-01 21:04:28 +0000

    added more list feature in cli

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ Bundler.require
 
 require_relative '../lib/top_ten_islands_cli_gem/island.rb'
 require_relative '../lib/top_ten_islands_cli_gem/scraper.rb'
+require_relative '../lib/top_ten_islands_cli_gem/cli.rb'
--- lib/top_ten_islands_cli_gem/cli.rb
+++ lib/top_ten_islands_cli_gem/cli.rb
@@ -1,2 +1,21 @@
 class TopTenIslandsCliGem::Cli
+
+  def start
+    input = ""
+    while input == ""
+      puts "Press a key to find out the the top 10 islands"
+      input = gets.chomp
+      list
+      #discription
+    end
+  end
+
+  def list
+    TopTenIslandsCliGem::Island.all.each.with_index(1) {|island, index| puts "#{index}. #{island.name}"}
+  end
+
+
+
+
+
 end
