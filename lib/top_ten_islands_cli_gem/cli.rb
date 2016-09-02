499a4087bf6992f0dc738a0898a09855e1d93455
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-09-02 14:39:39 +0000

    added discription method and added whitspace in cli

--- lib/top_ten_islands_cli_gem/cli.rb
+++ lib/top_ten_islands_cli_gem/cli.rb
@@ -1,17 +1,31 @@
 class TopTenIslandsCliGem::Cli
 
   def start
-    input = ""
-    while input == ""
-      puts "Press a key to find out the the top 10 islands"
-      input = gets.chomp
+      puts " "
+      puts "Welcome to the Worlds Top Islands!!"
+      puts " "
       list
-      #discription
-    end
+      discription
   end
 
   def list
     TopTenIslandsCliGem::Island.all.each.with_index(1) {|island, index| puts "#{index}. #{island.name}"}
+  end
+
+  def discription
+    input = 0
+    #binding.pry
+    until input.between?(1,10)
+      puts " "
+      puts "What number island would you like to read about?"
+      input = gets.chomp.to_i
+    end
+    #binding.pry
+    puts ""
+    TopTenIslandsCliGem::Island.all[input-1].name
+    puts ""
+    puts TopTenIslandsCliGem::Island.all[input-1].get_discription
+
   end
 
 
