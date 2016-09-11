14da946139912fb3745c1ab89ad8ebe5c59e8607
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-09-11 20:32:07 +0000

    scraper takes url and returns discription

--- lib/top_ten_islands_cli_gem/scraper.rb
+++ lib/top_ten_islands_cli_gem/scraper.rb
@@ -2,20 +2,16 @@ class TopTenIslandsCliGem::Scraper
 
   def self.get_page
     Nokogiri::HTML(open("https://www.tripadvisor.com/TravelersChoice-Islands"))
-    #binding.pry
   end
 
   def self.create_islands
-    #binding.pry
     island_array = get_page.css(" .mainName").map do |island|
        i = TopTenIslandsCliGem::Island.new(island.text.strip,"https://www.tripadvisor.com#{island.css("a").attribute("href").text}")
     end
   end
 
-  
-
-
-
-
+  def self.get_island_discription(url=nil)
+    Nokogiri::HTML(open(url)).css(" .ermb_text").css(" .content").text.strip
+  end
 
 end
