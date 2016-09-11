90be5604b2a2ebb4502b20eecd56227c65a983bc
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-09-11 20:27:33 +0000

    setup enviroment in lib

--- lib/top_ten_islands_cli_gem.rb
+++ lib/top_ten_islands_cli_gem.rb
@@ -1,9 +1,11 @@
-#require "open-uri"
-#require 'nokogiri'
-#require 'rake'
+require "open-uri"
+require "pry"
+require "nokogiri"
 
+require_relative "top_ten_islands_cli_gem/version"
+require_relative "top_ten_islands_cli_gem/scraper.rb"
+require_relative "top_ten_islands_cli_gem/island.rb"
+require_relative "top_ten_islands_cli_gem/cli.rb"
 
 module TopTenIslandsCliGem
 end
-
-require_relative '../config/environment'
