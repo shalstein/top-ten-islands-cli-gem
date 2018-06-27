836cff0fe557274d354a74130dadf68d7e1a9fd8
Sholom <boruchsteinmetz@outlook.com>
2018-06-26 20:19:53 -0400

    modify island class to have index property and changed data source URL

--- lib/top_ten_islands_cli_gem/island.rb
+++ lib/top_ten_islands_cli_gem/island.rb
@@ -1,28 +1,28 @@
 class TopTenIslandsCliGem::Island
 
-  attr_accessor :name, :url, :discription
+  attr_accessor :name, :index, :discription
 
   ALL = []
 
-  def initialize(name=nil, url=nil, discription=nil)
+  def initialize(name=nil, index=nil, discription=nil)
     @name = name
-    @url = url
+    @index = index
     ALL.push(self)
   end
 
   def get_discription
-    @discription ||= TopTenIslandsCliGem::Scraper.get_island_discription(@url)
+    @discription ||= TopTenIslandsCliGem::Scraper.get_island_discription(@index)
   end
 
   def self.all
-    ALL.reverse
+    ALL
   end
 
   def self.create_islands_from_hash
     islands_array = TopTenIslandsCliGem::Scraper.get_islands_array
 
     islands_array.each do |island_hash|
-      islands = self.new(island_hash[:name], island_hash[:url])
+      islands = self.new(island_hash[:name], island_hash[:index])
     end
 
   end
--- lib/top_ten_islands_cli_gem/scraper.rb
+++ lib/top_ten_islands_cli_gem/scraper.rb
@@ -2,25 +2,29 @@ class TopTenIslandsCliGem::Scraper
 
   def self.get_page
     # Nokogiri::HTML(open("https://www.tripadvisor.com/TravelersChoice-Islands"))
-    Nokogiri::HTML(open("https://www.travelandleisure.com/worlds-best/islands"))
+    Nokogiri::HTML(open("https://www.lonelyplanet.com/campaign/island-guide-2018/top-10-islands-for-2018/"))
     
   end
 
   def self.get_islands_array
     islands_array = []
 
-    get_page.css('.media-body.clearfix > h2')
-    .each do |island|
+    @@top_islands_page = self.get_page.css('.article__content')
+    
+    @@top_islands_page.css(' h3 > strong')
+      .each_with_index do |island, index|
        island_hash = {}
-       island_hash[:name] = island.text.strip
-      #  island_hash[:url] = "https://www.tripadvisor.com#{island.css("a").attribute("href").text}"
+       island_hash[:name] = island.text
+       island_hash[:index] = index
        islands_array.push(island_hash)
     end
     islands_array
   end
 
-  def self.get_island_discription(url=nil)
-    Nokogiri::HTML(open(url)).css(" .ermb_text").css(" .content").text.strip
+  def self.get_island_discription(index)
+    # Nokogiri::HTML(open(url)).css(" .ermb_text").css(" .content").text.strip
+        
+    @@top_islands_page.css('p')[index].text.strip
   end
 
 end
